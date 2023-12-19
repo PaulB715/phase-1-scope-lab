b295c9aed737599090e65ceb734b582ee6ebac21
Paul Brown <paulbrown389@gmail.com>
2023-12-18 21:34:51 -0500

    Scope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function getCustomerName() {
+  return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer
+}
+function changeLeastFavoriteCustomer(newCustomer) {
+    if (!newCustomer) {
+      throw new Error("Assignment to constant variable.");
+    }
+    let leastFavoriteCustomer = newCustomer;
+  }
